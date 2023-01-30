79fe3dfa9e43a5e061145823a7148efe7099404a
Santiago Gonzalez <parrasanti.gonzalez@gmail.com>
2023-01-29 20:19:06 -0500

    Estancado en el mismo problema

--- 03.py
+++ 03.py
@@ -5,6 +5,12 @@ Created on Sat Jan 28 14:42:51 2023
 @author: Usuario
 """
 
+caracteres_permitidos = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'á', 'é', 'í', 'ó', 'ú', 'ñ']
+texto = 'Muchos años después frente al pelotón de fusilamiento el coronel Aureliano Buendía había de recordar aquella tarde remota en que su padre lo llevó a conocer el hielo'
+
+for i in range(len(texto)):
+    print(f"indice: {i} letra: {texto[i]}")
+
 def convertir_a_tupla(lista:list)->tuple:
     
     tupla_retorno = tuple(lista)
@@ -12,42 +18,24 @@ def convertir_a_tupla(lista:list)->tuple:
     return tupla_retorno
 
 
-
-
-caracteres_permitidos = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'á', 'é', 'í', 'ó', 'ú']
-texto = 'Muchos años después frente al pelotón de fusilamiento el coronel Aureliano Buendía había de recordar aquella tarde remota en que su padre lo llevó a conocer el hielo'
-
-texto = texto.lower()
-
-for letra in texto:
-    if letra not in caracteres_permitidos:
-        texto = texto.replace(letra, ' ')
+def analizar_texto(texto: str, caracteres_permitidos: list) -> dict:
+    diccionario = {}
+    texto = texto.lower()
     
-lista = texto.split(' ')
-
-print(lista)
-
-
-diccionario = {}
-
-for palabra in lista:
-    
-    lista_salida = []
-    comprobada = True
-    
-    for letra in palabra:
+    for letra in texto:
         if letra not in caracteres_permitidos:
-            comprobada = False
-            
-    if comprobada == True:
-        diccionario[palabra] = [lista.count(palabra), texto.find(palabra) , texto.rfind(palabra)]
+            texto = texto.replace(letra, ' ')
+    
+    if len(texto) <= 0 or len(caracteres_permitidos) <= 0:
+        diccionario = diccionario
     else:
-        diccionario[palabra] = [lista.count(palabra), 3]
-    
-for palabra in diccionario:
-    lista = diccionario[palabra]
-    
-    diccionario[palabra] = convertir_a_tupla(lista)
-    
+        
+        palabras_texto = texto.split(' ')
+        
+        for palabra in palabras_texto:
+            
+            diccionario[palabra] = [palabras_texto.count(palabra), texto.find(palabra), texto.rindex(palabra)]
 
-# print(diccionario)
+    return diccionario
+
+# print(analizar_texto(texto, caracteres_permitidos))
